33fe791ed8725d6be2c032c21d9213b3fee9233a
Alexey Berdnikov <aberdnikov@gmail.com>
2017-06-08 15:31:51 +0500

    fixed

--- init.php
+++ init.php
@@ -2,5 +2,5 @@
 \Larakit\StaticFiles\Manager::package('larakit/sf-angular-yamap')
     ->usePackage('larakit/sf-angular')
     ->setSourceDir('public')
-    ->jsPackage('ya-map-2.1.min.js')
-;
+    ->jsPackage('ya-map-2.1.min.js');
+\Larakit\NgAdminlte\LkNg::moduleRegister('yaMap');
